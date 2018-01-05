2cab4c9e99e39fffa8e910124ad20dbc44012f21
Adam Carmichael <carneeki@carneeki.net>
2018-01-06 05:30:01 +1100

    write outputs to file rather that STDOUT

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ extern crate meval;
 extern crate itertools;
 
 use std::io::{self, BufRead};
+use std::io::prelude::*;
 use std::io::BufReader;
+use std::io::BufWriter;
 use std::path::Path;
 use std::fs::File;
 use itertools::Itertools;
@@ -34,46 +36,45 @@ fn generate(m_opts: clap::ArgMatches)
     {
         let lib = m_opts.value_of("lib").unwrap();
         let gb_ratios = m_opts.value_of("gb_ratios").unwrap();
-        let m :f64 = m_opts.value_of("gear_m").unwrap().parse::<f64>().unwrap();  // TODO dafuq?
-        let n :f64 = m_opts.value_of("gear_n").unwrap().parse::<f64>().unwrap();  // TODO same
-        let pitch :f64 = m_opts.value_of("pitch").unwrap().parse::<f64>().unwrap(); // TODO and me
-        let out = m_opts.value_of("out").unwrap();
+        let m: f64 = m_opts.value_of("gear_m").unwrap().parse::<f64>().unwrap();  // TODO dafuq?
+        let n: f64 = m_opts.value_of("gear_n").unwrap().parse::<f64>().unwrap();  // TODO same
+        let pitch: f64 = m_opts.value_of("pitch").unwrap().parse::<f64>().unwrap(); // TODO and me
 
-        let gears :Vec<i32> = gears_from_file(lib);
-        let ratios :Vec<f64> = ratios_from_file(gb_ratios);
+        // file output (w/buffering)
+        let out_str = m_opts.value_of("out").unwrap();
+        let mut out_file = match File::create(Path::new(out_str))
+        {
+            Ok(out_file) => out_file,
+            Err(e) => panic!("Couldn't open output file: {:?}", e),
+        };
+        let mut out = BufWriter::new(&out_file);
 
-/*
-        println!("lib: {:?}", lib);
-        println!("gb_ratios: {:?}", gb_ratios);
-        println!("m: {:?}", m);
-        println!("n: {:?}", n);
-        println!("out: {:?}", out);
-        println!("pitch: {:?}", pitch);
-        println!("gears: {:?}", gears);
-        println!("ratios: {:?}", ratios);
-*/
+        let gears: Vec<i32> = gears_from_file(lib);
+        let ratios: Vec<f64> = ratios_from_file(gb_ratios);
 
+        let mnp: f64 = (m/n)*pitch;
+
+        // iterate every ab combination
         for combos in gears.clone().into_iter().combinations(2)
         {
-            // println!("{:?}", combos);
-
             let a = combos[0] as f64;
             let b = combos[1] as f64;
 
             // generate a list with 2 gears
             // a/b
             // b/a
-            for r in ratios.clone().into_iter()
+            for r in &ratios
             {
-                print_ab(m, n, a, b, r, pitch, (m/n) * ( a/b ) * r * pitch );
-                print_ab(m, n, b, a, r, pitch, (m/n) * ( b/a ) * r * pitch );
+                let mut ans = (mnp * (a/b) * r);
+                out.write_fmt(format_args!("({:03}/{:03}) * ({:03} / {:03}) * {:2.5} * {:?} = {:2.5}mm ({:2.5} TPI)\n", m, n, a, b, r, pitch, ans, 25.4/ans ));
+                ans = (mnp * (b/a) * r);
+                out.write_fmt(format_args!("({:03}/{:03}) * ({:03} / {:03}) * {:2.5} * {:?} = {:2.5}mm ({:2.5} TPI)\n", m, n, a, b, r, pitch, ans, 25.4/ans ));
             }
         }
 
+        // iterate every abcd combination
         for combos in gears.clone().into_iter().combinations(4)
         {
-            //println!("{:?}", combos);
-
             let a = combos[0] as f64;
             let b = combos[1] as f64;
             let c = combos[3] as f64;
@@ -87,28 +88,38 @@ fn generate(m_opts: clap::ArgMatches)
             // bc / ad
             // bd / ac
             // cd / ab
-            for r in ratios.clone().into_iter()
+            for r in &ratios
             {
-                print_abcd(m, n, a, c, b, d, r, pitch, (m/n) * ( (a*c) / (b*d)) * r * pitch );
-                print_abcd(m, n, a, b, c, d, r, pitch, (m/n) * ( (a*b) / (c*d)) * r * pitch );
-                print_abcd(m, n, a, d, b, c, r, pitch, (m/n) * ( (a*d) / (b*c)) * r * pitch );
-                print_abcd(m, n, b, c, a, d, r, pitch, (m/n) * ( (b*c) / (a*d)) * r * pitch );
-                print_abcd(m, n, b, d, a, c, r, pitch, (m/n) * ( (b*d) / (a*c)) * r * pitch );
-                print_abcd(m, n, c, d, a, b, r, pitch, (m/n) * ( (c*d) / (a*b)) * r * pitch );
+                let mut ans;
+                ans = (mnp * ((a*c)/(b*d)) * r);
+                out.write_fmt(format_args!("({:03}/{:03}) * [({:03} * {:03}) / ({:03} * {:03})] * {:2.5} * {:?} = {:2.5}mm ({:2.5} TPI)\n", m, n, a, c, b, d, r, pitch, ans, 25.4/ans) );
+                ans = (mnp * ((a*b)/(c*d)) * r);
+                out.write_fmt(format_args!("({:03}/{:03}) * [({:03} * {:03}) / ({:03} * {:03})] * {:2.5} * {:?} = {:2.5}mm ({:2.5} TPI)\n", m, n, a, b, c, d, r, pitch, ans, 25.4/ans) );
+                ans = (mnp * ((a*d)/(b*c)) * r);
+                out.write_fmt(format_args!("({:03}/{:03}) * [({:03} * {:03}) / ({:03} * {:03})] * {:2.5} * {:?} = {:2.5}mm ({:2.5} TPI)\n", m, n, a, d, b, c, r, pitch, ans, 25.4/ans) );
+                ans = (mnp * ((b*c)/(a*d)) * r);
+                out.write_fmt(format_args!("({:03}/{:03}) * [({:03} * {:03}) / ({:03} * {:03})] * {:2.5} * {:?} = {:2.5}mm ({:2.5} TPI)\n", m, n, b, c, a, d, r, pitch, ans, 25.4/ans) );
+                ans = (mnp * ((b*d)/(a*c)) * r);
+                out.write_fmt(format_args!("({:03}/{:03}) * [({:03} * {:03}) / ({:03} * {:03})] * {:2.5} * {:?} = {:2.5}mm ({:2.5} TPI)\n", m, n, b, d, a, c, r, pitch, ans, 25.4/ans) );
+                ans = (mnp * ((c*d)/(a*b)) * r);
+                out.write_fmt(format_args!("({:03}/{:03}) * [({:03} * {:03}) / ({:03} * {:03})] * {:2.5} * {:?} = {:2.5}mm ({:2.5} TPI)\n", m, n, c, d, a, b, r, pitch, ans, 25.4/ans) );
+                /*
+                print_abcd(&m, &n, &a, &c, &b, &d, &r, &pitch, mnp * ( (a*c) / (b*d)) * r );
+                print_abcd(&m, &n, &a, &b, &c, &d, &r, &pitch, mnp * ( (a*b) / (c*d)) * r );
+                print_abcd(&m, &n, &a, &d, &b, &c, &r, &pitch, mnp * ( (a*d) / (b*c)) * r );
+                print_abcd(&m, &n, &b, &c, &a, &d, &r, &pitch, mnp * ( (b*c) / (a*d)) * r );
+                print_abcd(&m, &n, &b, &d, &a, &c, &r, &pitch, mnp * ( (b*d) / (a*c)) * r );
+                print_abcd(&m, &n, &c, &d, &a, &b, &r, &pitch, mnp * ( (c*d) / (a*b)) * r );
+                */
             }
         }
     }
 }
 
-fn print_ab(m: f64, n: f64, a: f64, b: f64, r: f64, pitch: f64, ans: f64)
-{
-    println!("({:03}/{:03}) * ({:03} / {:03}) * {:2.5} * {:?} = {:2.5}", m, n, a, b, r, pitch, ans );
-}
-
-fn print_abcd(m: f64, n: f64, a: f64, b: f64, c: f64, d: f64, r: f64, pitch: f64, ans: f64)
-{
-    println!("({:03}/{:03}) * [({:03} * {:03}) / ({:03} * {:03})] * {:2.5} * {:?} = {:2.5}", m, n, a, c, b, d, r, pitch, ans );
-}
+//fn print_abcd(m: &f64, n: &f64, a: &f64, b: &f64, c: &f64, d: &f64, r: &f64, pitch: &f64, ans: f64)
+//{
+    //println!("({:03}/{:03}) * [({:03} * {:03}) / ({:03} * {:03})] * {:2.5} * {:?} = {:2.5}", m, n, a, c, b, d, r, pitch, ans );
+//}
 
 fn lookup(m: clap::ArgMatches)
 {
